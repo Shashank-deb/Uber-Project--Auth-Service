0edc1a146567d912f35ea5e2485eb677b4a736f4
Shashank Sharma <sharmashashank446@gmail.com>
2025-12-13 18:08:53 +0530

    Adding Starter files of Auth Microservice

---
+++ src/main/java/com/example/uberprojectauthservice/dto/PassengerDTO.java
@@ -0,0 +1,20 @@
+package com.example.uberprojectauthservice.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerDTO {
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private Date createdAt;
+}
---
+++ src/main/java/com/example/uberprojectauthservice/dto/PassengerSignUpRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.uberprojectauthservice.dto;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerSignUpRequestDTO {
+
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String name;
+}
