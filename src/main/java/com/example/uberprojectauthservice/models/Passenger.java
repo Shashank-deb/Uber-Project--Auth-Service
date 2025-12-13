018675fe37a6c3773e86079179f375c8d6d793cf
Shashank Sharma <sharmashashank446@gmail.com>
2025-12-13 20:31:11 +0530

    Copy the migration and model, and add phone number, email, and password to the Passenger model

---
+++ src/main/java/com/example/uberprojectauthservice/models/Passenger.java
@@ -0,0 +1,36 @@
+package com.example.uberprojectauthservice.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","bookings"})
+public class Passenger extends BaseModel {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "passenger")
+    private List<Booking> bookings = new ArrayList<>();
+}
